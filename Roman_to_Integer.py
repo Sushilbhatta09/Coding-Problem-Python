b03bc5fa02d413e5f185306f85c515332bfca368
Sushilbhatta09 <87477889+Sushilbhatta09@users.noreply.github.com>
2023-07-09 13:13:47 +0545

    first commit

---
+++ Roman_to_Integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def romanToInt(self, s):
+        map={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace("XL", "XXXX").replace("XC", "LXXXX").replace("CD", "CCCC").replace("CM", "DCCCC")
+        sum=0
+        for x in s:
+            sum+=map[x]
+        return sum
